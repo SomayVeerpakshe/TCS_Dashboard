c0f3c062738ec4c0d21e5c8286ddf3e5275bb2d3
Somay Veerpakshe <vsomay04@gmail.com>
2025-05-03 13:15:46 +0530

    Update streamlit_learning.py

--- streamlit_learning.py
+++ streamlit_learning.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
 st.title("Streamlit")
-st.write("Hello Pune")
+st.write("Hello Python")
 st.balloons()
 st.image("https://www.streamlit.io/images/brand/streamlit-mark-color.png")
+x = st.slider("Select number",min_value=1,max_value=10)
+st.write("The square of ",x,"is", x*x)
